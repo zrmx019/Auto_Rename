98dbc86605680efbf02db448efef913790141dfe
zrmx019 <mxzrx831@gmail.com>
2025-04-17 20:29:46 +0530

    Update start_&_cb.py

--- start_&_cb.py
+++ start_&_cb.py
@@ -1,1 +1,209 @@
+import random import asyncio from pyrogram import Client, filters from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+from helper.database import codeflixbots from config import * from config import Config
+
+Start Command Handler
+
+@Client.on_message(filters.private & filters.command("start")) async def start(client, message: Message): user = message.from_user await codeflixbots.add_user(client, message)
+
+# Initial interactive text and sticker sequence
+m = await message.reply_text("ʜᴇʜᴇ..ɪ'ᴍ ᴀɴʏᴀ!\nᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ. . .")
+await asyncio.sleep(0.4)
+await m.edit_text("🎊")
+await asyncio.sleep(0.5)
+await m.edit_text("⚡")
+await asyncio.sleep(0.5)
+await m.edit_text("ᴡᴀᴋᴜ ᴡᴀᴋᴜ!...")
+await asyncio.sleep(0.4)
+await m.delete()
+
+# Send sticker after the text sequence
+await message.reply_sticker("CAACAgUAAxkBAAECroBmQKMAAQ-Gw4nibWoj_pJou2vP1a4AAlQIAAIzDxlVkNBkTEb1Lc4eBA")
+
+# Define buttons for the start message
+buttons = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton("• ᴍʏ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs •", callback_data='help')
+    ],
+    [
+        InlineKeyboardButton('• ᴜᴘᴅᴀᴛᴇs', url='https://t.me/weoo_animes'),
+        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ •', url='https://t.me/weoo_chats')
+    ],
+    [
+        InlineKeyboardButton('• ᴀʙᴏᴜᴛ', callback_data='about'),
+        InlineKeyboardButton('sᴏᴜʀᴄᴇ •', callback_data='source')
+    ]
+])
+
+# Send start message with or without picture
+if Config.START_PIC:
+    await message.reply_photo(
+        Config.START_PIC,
+        caption=Txt.START_TXT.format(user.mention),
+        reply_markup=buttons
+    )
+else:
+    await message.reply_text(
+        text=Txt.START_TXT.format(user.mention),
+        reply_markup=buttons,
+        disable_web_page_preview=True
+    )
+
+Callback Query Handler
+
+@Client.on_callback_query() async def cb_handler(client, query: CallbackQuery): data = query.data user_id = query.from_user.id
+
+print(f"Callback data received: {data}")  # Debugging line
+
+if data == "home":
+    await query.message.edit_text(
+        text=Txt.START_TXT.format(query.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴍʏ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs •", callback_data='help')],
+            [InlineKeyboardButton('• ᴜᴘᴅᴀᴛᴇs', url='https://t.me/weoo_animes'), InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ •', url='https://t.me/weoo_chats')],
+            [InlineKeyboardButton('• ᴀʙᴏᴜᴛ', callback_data='about'), InlineKeyboardButton('sᴏᴜʀᴄᴇ •', callback_data='source')]
+        ])
+    )
+elif data == "caption":
+    await query.message.edit_text(
+        text=Txt.CAPTION_TXT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• sᴜᴘᴘᴏʀᴛ", url='https://t.me/weoo_chats'), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+        ])
+    )
+
+elif data == "help":
+    await query.message.edit_text(
+        text=Txt.HELP_TXT.format(client.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴀᴜᴛᴏ ʀᴇɴᴀᴍᴇ ғᴏʀᴍᴀᴛ •", callback_data='file_names')],
+            [InlineKeyboardButton('• ᴛʜᴜᴍʙɴᴀɪʟ', callback_data='thumbnail'), InlineKeyboardButton('ᴄᴀᴘᴛɪᴏɴ •', callback_data='caption')],
+            [InlineKeyboardButton('• ᴍᴇᴛᴀᴅᴀᴛᴀ', callback_data='meta'), InlineKeyboardButton('ᴅᴏɴᴀᴛᴇ •', callback_data='donate')],
+            [InlineKeyboardButton('• ʜᴏᴍᴇ', callback_data='home')]
+        ])
+    )
+
+elif data == "meta":
+    await query.message.edit_text(  # Change edit_caption to edit_text
+        text=Txt.SEND_METADATA,  # Changed from caption to text
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+        ])
+    )
+elif data == "donate":
+    await query.message.edit_text(
+        text=Txt.DONATE_TXT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ʙᴀᴄᴋ", callback_data="help"), InlineKeyboardButton("ᴏᴡɴᴇʀ •", url='https://t.me/otakukart7')]
+        ])
+    )
+elif data == "file_names":
+    format_template = await codeflixbots.get_format_template(user_id)
+    await query.message.edit_text(
+        text=Txt.FILE_NAME_TXT.format(format_template=format_template),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+        ])
+    )
+elif data == "thumbnail":
+    await query.message.edit_caption(
+        caption=Txt.THUMBNAIL_TXT,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+        ])
+    )
+elif data == "metadatax":
+    await query.message.edit_caption(
+        caption=Txt.SEND_METADATA,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+        ])
+    )
+elif data == "source":
+    await query.message.edit_caption(
+        caption=Txt.SOURCE_TXT,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="home")]
+        ])
+    )
+elif data == "premiumx":
+    await query.message.edit_caption(
+        caption=Txt.PREMIUM_TXT,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ʙᴀᴄᴋ", callback_data="help"), InlineKeyboardButton("ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", url='https://t.me/otakukart7')]
+        ])
+    )
+elif data == "plans":
+    await query.message.edit_caption(
+        caption=Txt.PREPLANS_TXT,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", url='https://t.me/Otakukart7')]
+        ])
+    )
+elif data == "about":
+    await query.message.edit_text(
+        text=Txt.ABOUT_TXT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• sᴜᴘᴘᴏʀᴛ", url='https://t.me/weoo_chats'), InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅs •", callback_data="help")],
+            [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/otakukart7'), InlineKeyboardButton("ɴᴇᴛᴡᴏʀᴋ •", url='https://t.me/weoo_anime')],
+            [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="home")]
+        ])
+    )
+elif data == "close":
+    try:
+        await query.message.delete()
+        await query.message.reply_to_message.delete()
+        await query.message.continue_propagation()
+    except:
+        await query.message.delete()
+        await query.message.continue_propagation()
+
+Donation Command Handler
+
+@Client.on_message(filters.command("donate")) async def donation(client, message): buttons = InlineKeyboardMarkup([ [InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="help"), InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url='https://t.me/otakukart7')] ]) yt = await message.reply_photo(photo='https://graph.org/file/1919fe077848bd0783d4c.jpg', caption=Txt.DONATE_TXT, reply_markup=buttons) await asyncio.sleep(300) await yt.delete() await message.delete()
+
+Premium Command Handler
+
+@Client.on_message(filters.command("premium")) async def getpremium(bot, message): buttons = InlineKeyboardMarkup([ [InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/otakukart7"), InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")] ]) yt = await message.reply_photo(photo='https://graph.org/file/feebef43bbdf76e796b1b.jpg', caption=Txt.PREMIUM_TXT, reply_markup=buttons) await asyncio.sleep(300) await yt.delete() await message.delete()
+
+Plan Command Handler
+
+@Client.on_message(filters.command("plan")) async def premium(bot, message): buttons = InlineKeyboardMarkup([ [InlineKeyboardButton("sᴇɴᴅ ss", url="https://t.me/otakukart7"), InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")] ]) yt = await message.reply_photo(photo='https://graph.org/file/8b50e21db819f296661b7.jpg', caption=Txt.PREPLANS_TXT, reply_markup=buttons) await asyncio.sleep(300) await yt.delete() await message.delete()
+
+Bought Command Handler
+
+@Client.on_message(filters.command("bought") & filters.private) async def bought(client, message): msg = await message.reply('Wait im checking...') replied = message.reply_to_message
+
+if not replied:
+    await msg.edit("<b>Please reply with the screenshot of your payment for the premium purchase to proceed.\n\nFor example, first upload your screenshot, then reply to it using the '/bought' command</b>")
+elif replied.photo:
+    await client.send_photo(
+        chat_id=LOG_CHANNEL,
+        photo=replied.photo.file_id,
+        caption=f'<b>User - {message.from_user.mention}\nUser id - <code>{message.from_user.id}</code>\nUsername - <code>{message.from_user.username}</code>\nName - <code>{message.from_user.first_name}</code></b>',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Close", callback_data="close_data")]
+        ])
+    )
+    await msg.edit_text('<b>Your screenshot has been sent to Admins</b>')
+
+@Client.on_message(filters.private & filters.command("help")) async def help_command(client, message): # Await get_me to get the bot's user object bot = await client.get_me() mention = bot.mention
+
+# Send the help message with inline buttons
+await message.reply_text(
+    text=Txt.HELP_TXT.format(mention=mention),
+    disable_web_page_preview=True,
+    reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton("• ᴀᴜᴛᴏ ʀᴇɴᴀᴍᴇ ғᴏʀᴍᴀᴛ •", callback_data='file_names')],
+        [InlineKeyboardButton('• ᴛʜᴜᴍʙɴᴀɪʟ', callback_data='thumbnail'), InlineKeyboardButton('ᴄᴀᴘᴛɪᴏɴ •', callback_data='caption')],
+        [InlineKeyboardButton('• ᴍᴇᴛᴀᴅᴀᴛᴀ', callback_data='meta'), InlineKeyboardButton('ᴅᴏɴᴀᴛᴇ •', callback_data='donate')],
+        [InlineKeyboardButton('• ʜᴏᴍᴇ', callback_data='home')]
+    ])
+)
 
